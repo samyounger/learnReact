450eaf81269cef108e4c27bcb0cf8b80b570fdd2
Sam Younger <sam.younger@gmail.com>
2017-05-06 13:30:12 +0100

    refactored app.jsx to components

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
 module.exports = {
-  entry: "./public/app.js",
+  entry: "./public/app.jsx",
   output: {
     path: __dirname,
     filename: "./public/bundle.js"
   },
   resolve: {
+    root: __dirname,
+    alias: {
+      Greeter: "public/components/Greeter.jsx",
+      GreeterMessage: "public/components/GreeterMessage.jsx",
+      GreeterForm: "public/components/GreeterForm.jsx"
+    },
     extensions: ["", ".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        loader: "babel-loader",
+        query: {
+          presets: [ "react", "es2015" ]
+        },
+        test: /\.jsx?$/,
+        excludes: /(node_modules|bower_components)/
+      }
+    ]
   }
 };
