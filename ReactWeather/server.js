0d601ae8c0b246bc078059b7e3f69c44420b0a9b
Sam Younger <sam.younger@gmail.com>
2017-05-15 17:49:18 +0100

    prep repo for Heroku

--- ReactWeather/server.js
+++ ReactWeather/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 
 const app = express();
+const PORT = process.env.PORT || 8000;
+
+// redirects to http if on https
+app.use(function(req, res, next) {
+  if (req.headers["x-forwarded-proto"] === "http") {
+    next();
+  } else {
+    res.redirect(`http://${req.hotname}${req.url}`);
+  }
+});
 
 app.use(express.static("public"));
 
-app.listen(8000, function() {
-  console.log("Express server is up on port 8000");
+app.listen(PORT, function() {
+  console.log(`Express server is up on port ${PORT}`);
 })
