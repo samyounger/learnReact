fbf88f4640a8b4a4fde7bc4a568e24222a5d080a
Sam Younger <sam.younger@gmail.com>
2017-05-15 19:46:41 +0100

    style about page and link to resources

--- ReactWeather/app/components/About.jsx
+++ ReactWeather/app/components/About.jsx
@@ -3,8 +3,15 @@ var React = require("react");
 var About =  (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+
+      <p>This is a ReactJS application developed under the tutelage of Andrew Meade, his Udemy video tutorial.</p>
+      <a href="https://www.udemy.com/the-complete-react-web-app-developer-course" target="_blank">Link to Andrew's tutorial</a>
+
+      <p>This application queries the OpenWetherMap.org api to return the temparature of any city searched for.</p>
+      <a href="http://openweathermap.org/">Link to openweathermap</a>
+
+      <p>This application was completed by me (Sam Younger). My git repository is located <a href="https://github.com/samyounger" target="_blak">HERE</a>.</p>
     </div>
   );
 };
