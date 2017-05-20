bb7a4c7bfce37b1942ea1d6b2d75f41fcd81a68b
samyounger <sam.younger@gmail.com>
2017-05-20 17:47:34 +0100

    first commit of React Timer

---
+++ ReactTimer/app/components/Navigation.jsx
@@ -0,0 +1,23 @@
+const React = require("react");
+const {Link, IndexLink} = require("react-router");
+
+var Navigation = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Timer App</li>
+          <li><IndexLink to="/" activeClassName="active-link">Timer</IndexLink></li>
+          <li><Link to="/" activeClassName="active-link">Countdown</Link></li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">Created by <span><a href="http://samyounger.com" target="_blank">Sam Younger</a></span></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Navigation;
---
+++ ReactTimer/webpack.config.js
@@ -0,0 +1,47 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: [
+    "script!jquery/dist/jquery.min.js",
+    "script!foundation-sites/dist/js/foundation.min.js",
+    "./app/app.jsx"
+  ],
+  externals: {
+    jquery: "jQuery"
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      "$": "jquery",
+      "jquery": "jquery"
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: "./public/bundle.js"
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main:              "app/components/Main.jsx",
+      applicationStyles: "app/styles/app.scss",
+      Navigation:        "app/components/Navigation.jsx"
+    },
+    extensions: ["", ".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        loader: "babel-loader",
+        query: {
+          presets: [ "react", "es2015", "stage-0" ]
+        },
+        test: /\.jsx?$/,
+        excludes: /(node_modules|bower_components)/
+      },{
+        loader: 'style-loader!css-loader',
+        test: /\.css$/
+      }
+    ]
+  },
+  devtool: "cheap-module-eval-source-map"
+};
