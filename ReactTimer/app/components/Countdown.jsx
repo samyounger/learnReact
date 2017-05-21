d506248b3c8ec3ad9f4ead815bfcf42c80033e4b
samyounger <sam.younger@gmail.com>
2017-05-21 20:21:37 +0100

    countdown and testing functionality

--- ReactTimer/app/components/Countdown.jsx
+++ ReactTimer/app/components/Countdown.jsx
@@ -1,14 +1,36 @@
-const React = require("react");
-const Clock = require("Clock");
+const React         = require("react");
+const Clock         = require("Clock");
 const CountdownForm = require("CountdownForm");
 
 var Countdown = React.createClass({
   getInitialState: function() {
-    return {count: 0};
+    return {
+      count: 0,
+      countdownStatus: "stopped"
+    };
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    if(this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case "started":
+          this.startTimer();
+          break;
+      }
+    }
+  },
+  startTimer: function() {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0,
+        countdownStatus: newCount >= 0 ? "started" : "stopped"
+      });
+    },1000);
   },
   handleSetCountdown: function(seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: "started"
     });
   },
   render: function() {
